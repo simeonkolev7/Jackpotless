40ceb38295b1e3d0aa2229bd776f777b51b526ed
Simeonkolev <simeonkolev32@gmail.com>
2024-10-19 19:41:24 +0300

    Initial commit

---
+++ Assets/Scripts/DeckScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckScript : MonoBehaviour
+{
+    public Sprite[] cardSprites; // Array holding card sprites
+    int[] cardValues = new int[53]; // Array to store the values of the cards
+    int currentIndex = 0; // Track the current card being dealt
+
+    void Start()
+    {
+        GetCardValues(); // Initialize the values of the cards
+    }
+
+    // Assign values to the cards (e.g., 2-10, J, Q, K, A)
+    void GetCardValues()
+    {
+        int num = 0;
+        // Loop through each card sprite
+        for (int i = 0; i < cardSprites.Length; i++)
+        {
+            num = i % 13; // Reset after 13 cards (to handle 4 suits)
+
+            // Assign card value (Aces = 1, Face cards = 10)
+            if (num > 10 || num == 0)
+            {
+                num = 10; // Face cards and 10 have the same value
+            }
+            cardValues[i] = num++;
+        }
+    }
+
+    // Shuffle the deck by swapping cards randomly
+    public void Shuffle()
+    {
+        // Use Fisher-Yates shuffle algorithm
+        for (int i = cardSprites.Length - 1; i > 0; --i)
+        {
+            int j = Mathf.FloorToInt(Random.Range(0.0f, 1.0f) * (cardSprites.Length - 1)) + 1;
+            Sprite face = cardSprites[i];
+            cardSprites[i] = cardSprites[j];
+            cardSprites[j] = face;
+
+            int value = cardValues[i];
+            cardValues[i] = cardValues[j];
+            cardValues[j] = value;
+        }
+        currentIndex = 1; // Reset deck pointer to start after shuffling
+    }
+
+    // Deal a card, return its value
+    public int DealCard(CardScript cardScript)
+    {
+        // Assign the next card sprite and value to the cardScript object
+        cardScript.SetSprite(cardSprites[currentIndex]);
+        cardScript.SetValue(cardValues[currentIndex]);
+        currentIndex++; // Move to the next card
+        return cardScript.GetValueOfCard(); // Return the value of the dealt card
+    }
+
+    // Get the sprite for the back of the cards (used to hide the dealer's card)
+    public Sprite GetCardBack()
+    {
+        return cardSprites[0]; // Assuming the first sprite is the card back
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Game Buttons
+    public Button dealBtn;
+    public Button hitBtn;
+    public Button standBtn;
+
+    private int standClicks = 0;
+
+    // Access the player and dealer's script
+    public PlayerScript playerScript;
+    public PlayerScript dealerScript;
+
+    // public Text to access and update - hud
+    public Text scoreText;
+    public Text dealerScoreText;
+    public Text mainText;
+    public Text standBtnText;
+
+    // Card hiding dealer's 2nd card
+    public GameObject hideCard;
+
+    // Highscore and Win Count
+    private int winCount = 0;  // Tracks how many rounds the player has won
+    private int highScore = 0; // Tracks the player's highest score
+
+    // Text elements to display win count and high score
+    public Text winCountText;
+    public Text highScoreText;
+
+    void Start()
+    {
+        // Add on click listeners to the buttons
+        dealBtn.onClick.AddListener(() => DealClicked());
+        hitBtn.onClick.AddListener(() => HitClicked());
+        standBtn.onClick.AddListener(() => StandClicked());
+
+        // Initialize win count and high score
+        UpdateWinCountUI();
+        UpdateHighScoreUI();
+    }
+
+    private void DealClicked()
+    {
+        // Reset round, hide text, prep for new hand
+        playerScript.ResetHand();
+        dealerScript.ResetHand();
+        // Hide deal hand score at start of deal
+        dealerScoreText.gameObject.SetActive(false);
+        mainText.gameObject.SetActive(false);
+        dealerScoreText.gameObject.SetActive(false);
+        GameObject.Find("Deck").GetComponent<DeckScript>().Shuffle();
+        playerScript.StartHand();
+        dealerScript.StartHand();
+        // Update the scores displayed
+        scoreText.text = "Hand: " + playerScript.handValue.ToString();
+        dealerScoreText.text = "Hand: " + dealerScript.handValue.ToString();
+        // Place card back on dealer card, hide card
+        hideCard.GetComponent<Renderer>().enabled = true;
+        // Adjust buttons visibility
+        dealBtn.gameObject.SetActive(false);
+        hitBtn.gameObject.SetActive(true);
+        standBtn.gameObject.SetActive(true);
+        standBtnText.text = "Stand";
+    }
+
+    private void HitClicked()
+    {
+        // Check that there is still room on the table
+        if (playerScript.cardIndex <= 10)
+        {
+            playerScript.GetCard();
+            scoreText.text = "Hand: " + playerScript.handValue.ToString();
+            if (playerScript.handValue > 20) RoundOver();
+        }
+    }
+
+    private void StandClicked()
+    {
+        standClicks++;
+        if (standClicks > 1) RoundOver();
+        HitDealer();
+        standBtnText.text = "Call";
+    }
+
+    private void HitDealer()
+    {
+        while (dealerScript.handValue < 16 && dealerScript.cardIndex < 10)
+        {
+            dealerScript.GetCard();
+            dealerScoreText.text = "Hand: " + dealerScript.handValue.ToString();
+            if (dealerScript.handValue > 20) RoundOver();
+        }
+    }
+
+    // Check for winnner and loser, hand is over
+    void RoundOver()
+    {
+        // Booleans (true/false) for bust and blackjack/21
+        bool playerBust = playerScript.handValue > 21;
+        bool dealerBust = dealerScript.handValue > 21;
+        bool player21 = playerScript.handValue == 21;
+        bool dealer21 = dealerScript.handValue == 21;
+        // If stand has been clicked less than twice, no 21s or busts, quit function
+        if (standClicks < 2 && !playerBust && !dealerBust && !player21 && !dealer21) return;
+        bool roundOver = true;
+        // All bust, round over
+        if (playerBust && dealerBust)
+        {
+            mainText.text = "All Bust: No winners";
+        }
+        // if player busts, dealer wins
+        else if (playerBust || (!dealerBust && dealerScript.handValue > playerScript.handValue))
+        {
+            mainText.text = "Dealer wins!";
+        }
+        // if dealer busts or player wins
+        else if (dealerBust || playerScript.handValue > dealerScript.handValue)
+        {
+            mainText.text = "You win!";
+            // Increase win count and high score
+            winCount++;
+            highScore += 10; // Example: increment score by 10 for each win
+            UpdateWinCountUI();
+            UpdateHighScoreUI();
+        }
+        //Check for tie
+        else if (playerScript.handValue == dealerScript.handValue)
+        {
+            mainText.text = "Push: It's a tie!";
+        }
+        else
+        {
+            roundOver = false;
+        }
+        // Set UI up for next move / hand / turn
+        if (roundOver)
+        {
+            hitBtn.gameObject.SetActive(false);
+            standBtn.gameObject.SetActive(false);
+            dealBtn.gameObject.SetActive(true);
+            mainText.gameObject.SetActive(true);
+            dealerScoreText.gameObject.SetActive(true);
+            hideCard.GetComponent<Renderer>().enabled = false;
+            standClicks = 0;
+        }
+    }
+
+    // Update the win count text
+    private void UpdateWinCountUI()
+    {
+        winCountText.text = "Wins: " + winCount.ToString();
+    }
+
+    // Update the high score text
+    private void UpdateHighScoreUI()
+    {
+        highScoreText.text = "High Score: " + highScore.ToString();
+    }
+}
+
+
